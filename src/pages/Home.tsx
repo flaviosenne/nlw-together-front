62a288d0b02849f9cac9dee029b769ed5050cd25
Joao flavio <flaviosenne123@gmail.com>
2021-06-23 14:53:47 -0300

    thrid class

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent } from 'react'
 import {useHistory} from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
@@ -9,17 +10,32 @@ import { Button } from '../components/Button'
 import '../styles/auth.scss'
 
 import { useAuth } from '../hooks/useAuth'
+import { useState } from 'react'
+import { database } from '../services/firebase'
 
 export function Home(){
 
     const history = useHistory()
     const {user, signInWithGoogle} = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom(){
         if(!user){
             await signInWithGoogle()
         }
         history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent){
+        event.preventDefault()
+
+        if(roomCode.trim() === '') return
+    
+        const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+        if(!roomRef.exists()) return alert('sala não existe')
+
+        history.push(`/rooms/${roomCode}`)
     }
 
     return (
@@ -36,10 +52,12 @@ export function Home(){
                     Crie sua sala com o Google
                 </button>
                 <div className='separator'>ou entre em uma sala</div>
-                <form>
+                <form onSubmit={handleJoinRoom}>
                     <input 
                     type='text'
                     placeholder='digite o código da sala'
+                    onChange={e => setRoomCode(e.target.value)}
+                    value={roomCode}
                     />
                     <Button type='submit'>
                         Entrar na sala
---
+++ src/pages/Room.tsx
@@ -0,0 +1,122 @@
+import { useEffect } from 'react'
+import { FormEvent, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import logoImg from '../assets/images/logo.svg'
+import {Button }from '../components/Button'
+import {RoomCode} from '../components/RoomCode'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+import '../styles/room.scss'
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string
+        avatar: string
+    },
+    content: string,
+    isAnswered: boolean,
+    isHighlighted: boolean
+}>
+type RoomParams = {
+    id: string
+}
+
+type Questions = {
+    id: string,
+    author: {
+        name: string
+        avatar: string
+    },
+    content: string,
+    isAnswered: boolean,
+    isHighlighted: boolean
+}
+export function Room(){
+    const {user} = useAuth()
+    const params = useParams<RoomParams>()
+    const [newQuestion, setNewQuestion] = useState('')
+    const roomId = params.id
+    const [questions, setQuestions] = useState<Questions[]>([])
+    const [title, setTitle] = useState()
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomId}`)
+
+        roomRef.on('value', room => {
+            const databaseRoom = room.val()
+            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key,value]) => {
+                return {
+                    id: key,
+                    content: value.content,
+                    author: value.author,
+                    isHighlighted: value.isHighlighted,
+                    isAnswered: value.isAnswered
+                }
+            })
+
+            setTitle(databaseRoom.title)
+            setQuestions(parsedQuestions)
+        })
+    }, [roomId])
+
+    async function handleSendQuestion(e: FormEvent){
+        e.preventDefault()
+        if(newQuestion.trim() === '') return
+
+        if(!user) throw new Error('usuário não está ligado')
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar:user.avatar
+            },
+            isHighlighted:false,
+            isAnswered: false
+        }
+
+        await database.ref(`rooms/${roomId}/questions`).push(question)
+
+        setNewQuestion('')
+    }
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt='logo'/>
+                    <RoomCode code= {roomId}/>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+                </div>
+
+                <form onSubmit={handleSendQuestion}>
+                    <textarea 
+                    placeholder='o que você quer perguntar?'
+                    value={newQuestion}
+                    onChange={e => setNewQuestion(e.target.value)}
+                    />
+                    <div className="form-footer">
+                        {user ? (
+                        <div className='user-info'>
+                            <img src={user.avatar} alt={user.name}/>
+                            <span>{user.name}</span>
+                        </div>):
+                         (
+                            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        )}
+                        <Button type='submit' disabled={!user}>Enviar Pergunta</Button>
+                    </div>
+                </form>
+
+
+            </main>
+        </div>
+    )
+}
